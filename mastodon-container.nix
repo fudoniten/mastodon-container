18b5cbfe72a09d5876f5f2f3b45f23c82d78b1e0
niten <niten@fudo.org>
2023-07-26 17:13:21 -0700

    Another $connection_upgrade

--- mastodon-container.nix
+++ mastodon-container.nix
@@ -86,7 +86,7 @@ let
           proxy_redirect off;
           proxy_http_version 1.1;
           proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection $connection_upgrade;
+          proxy_set_header Connection "upgrade";
           tcp_nodelay on;
         }
       }
