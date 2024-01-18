7a071b4c8fb6c34112d53ae7caf2ef22bb5bbdb9
niten <niten@fudo.org>
2024-01-18 13:22:16 -0800

    Add logging, and recommended nginx settings

--- mastodon-container.nix
+++ mastodon-container.nix
@@ -139,6 +139,17 @@ in {
 
     services.nginx = {
       enable = true;
+      commonHttpConfig = ''
+        log_format with_response_time '$remote_addr - $remote_user [$time_local] '
+                     '"$request" $status $body_bytes_sent '
+                     '"$http_referer" "$http_user_agent" '
+                     '"$request_time" "$upstream_response_time"';
+        access_log /var/log/nginx/access.log with_response_time;
+      '';
+      recommendedTlsSettings = true;
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
       virtualHosts."${cfg.hostname}" = {
         enableACME = true;
         forceSSL = true;
